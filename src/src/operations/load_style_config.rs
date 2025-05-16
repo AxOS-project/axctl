13e6272aa010417c386f8c66c190acd3ec37f04d
Ardox <ardox@axos-project.com>
2025-05-16 13:06:46 +0200

    Replaced constant by let variable

--- src/src/operations/load_style_config.rs
+++ src/src/operations/load_style_config.rs
@@ -5,20 +5,20 @@ pub async fn load_style_config() {
     let current_user: &str = &env::var("SUDO_USER").unwrap_or_else(|_| "Unknown".to_string());
 
     const CONFIG_DIR: &str = "/etc/skel/.config/*";
-    const TARGET_DIR: &str = &format!("/home/{}/.config/", current_user);
+    let target_dir: &str = &format!("/home/{}/.config/", current_user);
 
     // using rsync instead of cp because it is more efficient
     let output = std::process::Command::new("rsync")
         .arg("-a")
         .arg("--progress")
         .arg(CONFIG_DIR)
-        .arg(TARGET_DIR)
+        .arg(target_dir)
         .output()
         .expect("Failed to copy configuration files");
 
     if !output.status.success() {
         eprintln!("Error copying configuration files: {}", String::from_utf8_lossy(&output.stderr));
     } else {
-        println!("Successfully copied configuration files to {}.", TARGET_DIR);
+        println!("Successfully copied configuration files to {}.", target_dir);
     }
 }
