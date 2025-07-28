cd9c601447e9e43991b9ac729af1060db0178529
Ardox <ardox@axos-project.com>
2025-07-28 13:08:52 +0300

    Fixed the blank line under replaced lines problem

--- src/src/internal/utils.rs
+++ src/src/internal/utils.rs
@@ -4,9 +4,13 @@ pub fn is_run_with_root() -> bool {
     uid == 0
 }
 
-pub fn sed_in_file(file_path: &str, search: &str, replace: &str, ) -> Result<(), Box<dyn std::error::Error>> {
-    let file_content = std::fs::read_to_string(file_path)?;
-    let new_content = file_content.replace(search, replace);
+pub fn sed_in_file(file_path: &str, search: &str, replace: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let file = std::fs::read_to_string(file_path)?;
+    let lines: Vec<String> = file
+        .lines()
+        .map(|line| line.replace(search, replace))
+        .collect();
+    let new_content = lines.join("\n");
     std::fs::write(file_path, new_content)?;
     Ok(())
 }
