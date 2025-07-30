ad80f11a946cc9f32eddbd7e7fb5438de598f69f
Ardox <ardox@axos-project.com>
2025-07-30 23:17:48 +0300

    Moved apps.conf file copy

--- PKGBUILD
+++ PKGBUILD
@@ -1,6 +1,6 @@
 pkgname="axctl"
 pkgver=0.3.2
-pkgrel=2
+pkgrel=3
 pkgdesc="A simple command line tool to control AxOS"
 arch=("x86_64")
 license=("GPL")
--- src/src/operations/load_sleex_user_config.rs
+++ src/src/operations/load_sleex_user_config.rs
@@ -49,19 +49,20 @@ pub fn load_sleex_user_config() {
                             target_config,
                             String::from_utf8_lossy(&sync_cmd.stderr)
                         );
-                        let skel_apps_conf = format!("{}/hypr/apps.conf", config_dir);
-                        let user_apps_conf = target_config.join("hypr/apps.conf");
-                        if !user_apps_conf.exists() {
-                            if let Err(e) = fs::copy(&skel_apps_conf, &user_apps_conf) {
-                                eprintln!("Failed to copy apps.conf for user {:?}: {}", user_apps_conf, e);
-                            } else {
-                                println!("Copied apps.conf to {:?}", user_apps_conf);
-                            }
-                        }
                         continue;
                     }
 
-                    
+                    let skel_apps_conf = format!("{}/hypr/apps.conf", config_dir);
+                    let user_apps_conf = target_config.join("hypr/apps.conf");
+                    if !user_apps_conf.exists() {
+                        if let Err(e) = fs::copy(&skel_apps_conf, &user_apps_conf) {
+                            eprintln!("Failed to copy apps.conf for user {:?}: {}", user_apps_conf, e);
+                        } else {
+                            println!("Copied apps.conf to {:?}", user_apps_conf);
+                        }
+                    }
+
+
                     // Fix ownership: chown -R username:username ~/.config
                     if let Some(user) = path.file_name().and_then(|n| n.to_str()) {
                         let chown_status = Command::new("chown")
